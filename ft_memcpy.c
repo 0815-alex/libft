9077572c0bfd20a9ba22a73a625cf527d3c28be4
Alexander Stein <astein@student.42lisboa.com>
2023-04-12 16:57:47 +0100

    done for 2 day

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:06:37 by astein            #+#    #+#             */
-/*   Updated: 2023/04/12 16:12:31 by astein           ###   ########.fr       */
+/*   Updated: 2023/04/12 16:56:59 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	unsigned char		*d;
+	unsigned const char	*s;
 
+	d = dest;
+	s = src;
+	if (d == NULL && s == NULL)
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
-		(char *)dest[i] = (char *)src[i];
+		d[i] = s[i];
 		i++;
 	}
-	return (dest);
+	return (d);
 }
