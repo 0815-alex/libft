d71e4a43f925cc920e6825a8e39ca018989d1f29
astein <stein.alexander@posteo.de>
2023-04-17 18:03:37 +0100

    fixed stuff

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 20:34:04 by astein            #+#    #+#             */
-/*   Updated: 2023/04/14 12:31:32 by astein           ###   ########.fr       */
+/*   Updated: 2023/04/17 18:01:38 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 char	*ft_strdup(const char *s)
 {
-	size_t	i;
 	size_t	len;
 	char	*dup;
 
-	i = -1;
 	len = ft_strlen(s) + 1;
 	if (!s)
 		return (NULL);
