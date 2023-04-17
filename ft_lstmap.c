7967c1149051b4d28582b61bd209132627da6eb8
Alexander Stein <astein@student.42lisboa.com>
2023-04-17 16:57:19 +0100

    all good i guess

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 16:11:48 by astein            #+#    #+#             */
-/*   Updated: 2023/04/17 16:36:01 by astein           ###   ########.fr       */
+/*   Updated: 2023/04/17 16:57:07 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,14 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	t_list	*current;
 	void	*new_content;
 
 	new_lst = NULL;
-	current = new_lst;
 	while (lst)
 	{
 		new_content = (f(lst->content));
 		if (new_content)
-		{
-			if (current)
-			{
-				current->next = ft_lstnew(new_content);
-				current = current->next;
-			}
-			else
-			{
-				new_lst = ft_lstnew(new_content);
-				current = new_lst;
-			}
-		}
+			ft_lstadd_back(&new_lst, ft_lstnew(new_content));
 		else
 			del(lst->content);
 		lst = lst->next;
