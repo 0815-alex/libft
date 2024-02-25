fe51f0db74b5dc539ec15bd6fee2f2d1ce77900a
ahokcool <stein.alexander@posteo.de>
2024-02-25 15:14:20 +0000

    changed ft_matrix_dim.c

--- src/ft_matrix_dim.c
+++ src/ft_matrix_dim.c
@@ -6,26 +6,26 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 14:39:11 by astein            #+#    #+#             */
-/*   Updated: 2024/02/01 14:46:39 by astein           ###   ########.fr       */
+/*   Updated: 2024/02/01 17:12:20 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_printf.h"
 
-void	ft_matrix_dim(char **matrix, int *x, int *y)
-{
-	int	biggest_x;
-
-	biggest_x = 0;
-	*y = 0;
-	while (matrix[*y])
+	void	ft_matrix_dim(char **matrix, int *x, int *y)
 	{
-		*x = 0;
-		while (matrix[*y][*x])
-			(*x)++;
-		if (*x > biggest_x)
-			biggest_x = *x;
-		(*y)++;
+		int	biggest_x;
+
+		biggest_x = 0;
+		*y = 0;
+		while (matrix[*y])
+		{
+			*x = 0;
+			while (matrix[*y][*x])
+				(*x)++;
+			if (*x > biggest_x)
+				biggest_x = *x;
+			(*y)++;
+		}
+		*x = biggest_x;
 	}
-	*x = biggest_x;
-}
